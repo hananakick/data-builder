c0a4f8826f68216b4d8ec9e09a94404af26e370d
bbggkkk <bbggkkk@naver.com>
2025-05-27 16:03:27 +0900

    refactor: remove commented-out build function for clarity

--- .build/build.js
+++ .build/build.js
@@ -1,20 +1,3 @@
-// import { build } from 'esbuild';
-
-// async function buildAlohomoraCore() {
-//   try {
-//     await build({
-//       entryPoints: ['./src/index.ts'],
-//       outfile: './bundle/index.js',
-//       bundle: true,
-//       minify: true,
-//     });
-//     console.log('Alohomora Core built successfully.');
-//   } catch (error) {
-//     console.error('Error building Alohomora Core:', error);
-//     process.exit(1);
-//   }
-// }
-// buildAlohomoraCore();
 import { build } from 'esbuild';
 import { execSync } from 'child_process';
 import { rmSync } from 'fs';
